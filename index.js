0e2e5bbe8e5a3ea6baa9e213daa03ac0343cb8af
Lucas Wargha <war18001@byui.edu>
2019-11-15 16:40:54 -0700

    test

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, '/views'))
+  .use(express.static(path.join(__dirname, '/public')))
+  .set("views", __dirname + "/views")
   .set('view engine', 'ejs')
   .get('/getRate', function calculateRate (req, res) {
       let type = req.query.mail_type;
